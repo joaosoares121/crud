ed1cff71b83f5324059a599f5ff2133e5b772bf8
joaosoares121 <geral@w3b.pt>
2023-01-17 21:04:06 +0000

    Correção

--- backend/config.ts
+++ backend/config.ts
@@ -1,9 +1,8 @@
 import { Domain } from "domain";
 import firebase from "firebase/app";
 import 'firebase/firestore'
-
 // se for maior que 0 significa que foi inicializado
-if(!firebase.app.length){
+if(!firebase.apps.length){
     firebase.initializeApp({
         apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
